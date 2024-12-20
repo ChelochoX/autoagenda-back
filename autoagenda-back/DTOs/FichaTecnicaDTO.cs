1c8cb1b3fa25a8061308b6c725ab5c5947760def
Cesar Lezcano <cesarlezca@gmail.com>
2024-12-19 23:21:31 -0300

    Update FichaTecnicaDTO.cs

--- autoagenda-back/DTOs/FichaTecnicaDTO.cs
+++ autoagenda-back/DTOs/FichaTecnicaDTO.cs
@@ -4,18 +4,21 @@ public class FichaTecnicaDTO
 {
     public int IdFicha { get; set; }
     public int IdCita { get; set; }
-    public int IdUsuario { get; set; }
-    public int IdVehiculo { get; set; }
     public int KilometrajeIngreso { get; set; }
     public int KilometrajeProximo { get; set; }
     public required string DetallesServicio { get; set; }
     public required string MecanicoResponsable { get; set; }
     public required string Estado { get; set; }
     public DateTime FechaCreacion { get; set; }
+
+    // Datos del vehículo
     public required string PlacaVehiculo { get; set; }
     public int IdMarca { get; set; }
     public int IdModelo { get; set; }
-    public int IdAnho { get; set; }
+    public required string NombreMarca { get; set; }
+    public required string NombreModelo { get; set; }
+
+    // Datos del cliente
     public required string NombreCliente { get; set; }
     public required string CorreoCliente { get; set; }
     public required string TelefonoCliente { get; set; }
