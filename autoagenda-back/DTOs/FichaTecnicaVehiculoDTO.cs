070869ad9fb7797e57a616038af1401996c37f44
Cesar Lezcano <cesarlezca@gmail.com>
2025-01-07 12:08:58 -0300

    Create FichaTecnicaVehiculoDTO.cs

---
+++ autoagenda-back/DTOs/FichaTecnicaVehiculoDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace autoagenda_back.DTOs;
+
+public class FichaTecnicaVehiculoDTO
+{
+    public int IdFicha { get; set; }
+    public int KilometrajeIngreso { get; set; }
+    public int KilometrajeProximo { get; set; }
+    public required string DetallesServicio { get; set; }
+    public required string MecanicoResponsable { get; set; }
+    public required string Estado { get; set; }
+    public DateTime FechaCierre { get; set; }
+}
